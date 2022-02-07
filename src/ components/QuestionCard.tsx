50c98b8bdccdc7882e3d16c826b43ea6c3793e62
nikhil andola <nikhilandola123@gmail.com>
2022-02-07 23:44:59 +0530

    "added all styles"

--- src/ components/QuestionCard.tsx
+++ src/ components/QuestionCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AnswerObject } from "../App";
-
+// Styles
+import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
 interface PropType {
   question: string;
   answers: string[];
@@ -18,26 +19,33 @@ const QuestionCard: React.FC<PropType> = ({
   questionNo,
   totalQuestions,
 }) => (
-  <div>
+  <Wrapper>
     <p className="number">
       Question: {questionNo} / {totalQuestions}
     </p>
     <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
       {answers.map((answer) => (
-        <div key={answer}>
+        <ButtonWrapper
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+          key={answer}
+        >
           {/* The double exclamation point(!!), or double bang, converts a truthy or falsy value to “true” or “false”. In other words, it operates exactly like Boolean(value) would. */}
 
           {/* <button disabled={!!userAnswer} value={answer} onClick={callback}>  */}
-                                    {/* is similar to  */}
-          <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
-          
+          {/* is similar to  */}
+          <button
+            disabled={userAnswer ? true : false}
+            value={answer}
+            onClick={callback}
+          >
             <span dangerouslySetInnerHTML={{ __html: answer }} />
           </button>
-        </div>
+        </ButtonWrapper>
       ))}
     </div>
-  </div>
+  </Wrapper>
 );
 
 export default QuestionCard;
